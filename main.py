792eb9e11232b944339024822b1a2cb1fb9b71af
Neha-Sethumadhavan <nehasethu0@gmail.com>
2025-02-08 21:39:29 +0530

    hi

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+import random
 
 # creates a Flask application
 app = Flask(__name__)
@@ -12,6 +13,31 @@ def hello():
 @app.route("/this")
 def lmaoo():
 	return render_template('hello.html')
+
+@app.route('/getwords',methods=['GET','POST'])
+def home():
+    
+    file=open("t1.txt","r")
+    text=file.read().split(',')
+    words=list(text)
+
+
+    for i in range(30):
+        s=random.randint(0,30)
+        word1=text[s]
+        s=random.randint(0,30)
+        word2=text[s]
+        s=random.randint(0,30)
+        word3=text[s]
+               
+    if(request.method=='GET'):
+        data1="word1"
+        data2="word2"
+        data3="word3"
+        return jsonify({'word1':word1,
+                        'word2':word2,
+                        'word3':word3})
+
 
 if __name__ == "__main__":
 	app.run(debug=True)
